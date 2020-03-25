1c7936d87e4a7e7dc0a1deb366a0fd258a58eac4
Luis Carlos Kristen <luiskristen@gmail.com>
2020-03-25 22:16:40 +0100

    pagina fixed de conexion a JSON API

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -11,43 +11,47 @@ import '../assets/styles/App.scss';
 
 
 const App = () => {
-    const [ videos , setVideos ] = useState([]);    
-
+    const [ videos , setVideos ] = useState({
+        mylist: [],
+        trends: [],
+        originals:  []        
+    });  
     useEffect(() => {
         fetch('http://localhost:3000/initalState')
             .then(response => response.json())
             .then(data => setVideos(data));
-    }, []);
-    
+    }, []);    
     return (
-    <div className="App">
-        <Header />
-        <Search />
+        <div className="App">
+            <Header />
+            <Search />
 
-        {videos.mylist.length > 0 &&
-            <Categories title="Mi Lista">
+            {videos.mylist.length > 0 &&
+                <Categories title="Mi Lista">
+                    <Carousel>
+                        <CarouselItem />
+                    </Carousel>
+                </Categories>
+            }
+
+            <Categories title="Tendencias">
                 <Carousel>
-                    <CarouselItem />
+                    {videos.trends.map(item =>
+                        <CarouselItem key={item.id} {...item} />
+                )}
                 </Carousel>
-             </Categories>
-        }
+            </Categories>
 
-    <Categories title="Tendencias">
-        <Carousel>
-        {videos.trends.map(item =>
-            <CarouselItem key={item.id} {...item} />
-        )}
-        </Carousel>
-    </Categories>
-
-    <Categories title="originales de Platzi Video">
-        <Carousel>
-            <CarouselItem />
-        </Carousel>
-    </Categories>
-
-    <Footer />
+            <Categories title="originales de Platzi Video">
+                <Carousel>
+                    {videos.originals.map(item =>
+                        <CarouselItem key={item.id} {...item} />
+                    )}
+                </Carousel>
+            </Categories>
+        <Footer />
     </div>
+    
     )
 }
 export default App;
