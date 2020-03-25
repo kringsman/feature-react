ad2f53bebff44c43c68d87c70ba57d60530b8be8
Luis Carlos Kristen <luiskristen@gmail.com>
2020-03-25 18:49:22 +0100

    diseño de pagina sin API

---
+++ src/containers/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+import '../assets/styles/App.scss';
+
+
+
+const App = () => (
+    <div className="App">
+        <Header />
+        <Search />
+
+        <Categories title="Mi Lista">
+            <Carousel>
+                <CarouselItem />
+                <CarouselItem />
+                <CarouselItem />
+            </Carousel>
+        </Categories>
+
+        <Categories title="tendencias">
+        <Carousel>
+            <CarouselItem />
+            <CarouselItem />
+        </Carousel>
+    </Categories>
+
+    <Categories title="originales de Platzi Video">
+    <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+    </Carousel>
+</Categories>
+
+    <Footer />
+    </div>
+)
+
+export default App;
