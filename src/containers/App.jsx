e5a5528ddd9a0b00b958efc093a11f193829ae76
Luis Carlos Kristen <luiskristen@gmail.com>
2020-03-25 21:29:44 +0100

    diseño de platzi video con API, error de en conexion con API dice length

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,46 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+
 import '../assets/styles/App.scss';
 
 
 
-const App = () => (
+const App = () => {
+    const [ videos , setVideos ] = useState([]);    
+
+    useEffect(() => {
+        fetch('http://localhost:3000/initalState')
+            .then(response => response.json())
+            .then(data => setVideos(data));
+    }, []);
+    
+    return (
     <div className="App">
         <Header />
         <Search />
 
-        <Categories title="Mi Lista">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+        {videos.mylist.length > 0 &&
+            <Categories title="Mi Lista">
+                <Carousel>
+                    <CarouselItem />
+                </Carousel>
+             </Categories>
+        }
 
-        <Categories title="tendencias">
+    <Categories title="Tendencias">
         <Carousel>
-            <CarouselItem />
-            <CarouselItem />
+        {videos.trends.map(item =>
+            <CarouselItem key={item.id} {...item} />
+        )}
         </Carousel>
     </Categories>
 
     <Categories title="originales de Platzi Video">
-    <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-    </Carousel>
-</Categories>
+        <Carousel>
+            <CarouselItem />
+        </Carousel>
+    </Categories>
 
     <Footer />
     </div>
-)
-
+    )
+}
 export default App;
