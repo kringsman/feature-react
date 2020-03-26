0785cc4c74bc7626f7aa7e56aba1f5d822aac24d
Luis Carlos Kristen <luiskristen@gmail.com>
2020-03-26 12:21:54 +0100

    agregue PropTypes

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
+import Carousel from './Carousel';
 
 const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     <div className="carousel-item">
@@ -19,5 +21,11 @@ const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     </div>
 ); 
 
-
+CarouselItem.propTypes = {
+  cover: PropTypes.string,
+  title: PropTypes.string,
+  year: PropTypes.number,
+  contentRating: PropTypes.string,
+  duration: PropTypes.number,
+}
 export default CarouselItem;
