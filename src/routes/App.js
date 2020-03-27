131467ffe66f224c652b4deba31b2370bbc9493d
Luis Carlos Kristen <luiskristen@gmail.com>
2020-03-27 14:29:30 +0100

    interaccion del video implementando react Hooks

--- src/routes/App.js
+++ src/routes/App.js
@@ -4,7 +4,9 @@ import Home from '../containers/Home';
 import Login from '../containers/Login';
 import Register from '../containers/Register';
 import NotFound from '../containers/NotFound';
+import Player from '../containers/Player';
 import Layout from '../components/Layout';
+
 
 const App = () => (
     <BrowserRouter>
@@ -12,7 +14,8 @@ const App = () => (
     <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />        
+        <Route exact path="/register" component={Register} /> 
+        <Route exact path="/player/:id" component={Player} />      
         <Route component={NotFound} />        
 </Switch>    
     </Layout>
